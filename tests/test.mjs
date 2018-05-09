197d499a40b061fc9c73bc98fe60074f343e73e6
Zach Overhulser <zoverhulser@gmail.com>
2018-05-09 17:35:45 -0500

    Tests Spanish greeting.

--- tests/test.mjs
+++ tests/test.mjs
@@ -6,4 +6,5 @@ import {makeGreeting} from "../greeting";
 
 assert.strictEqual(makeGreeting(), "Hello world!");
 assert.strictEqual(makeGreeting("Mike"), "Hello Mike!");
+assert.strictEqual(makeGreeting("", "Spanish"), "¡Hola mundo!");
 console.log("All tests have passed.");

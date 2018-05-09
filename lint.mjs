97cfdb99f10b523b44d61ccb31f8b472e2b75803
Zach Overhulser <zoverhulser@gmail.com>
2018-05-09 17:09:50 -0500

    Imports in lint.mjs with ES6.

--- lint.mjs
+++ lint.mjs
@@ -1,13 +1,12 @@
 /*jslint
     node
 */
-const jslint = require("./jslint");
-const fs = require("fs");
+import jslint from "./jslint";
+import fs from "fs";
 
 const fileName = process.argv[2];
 
 fs.readFile(fileName, "utf-8", function (error, fileText) {
-    "use strict";
     if (error) {
         throw error;
     }

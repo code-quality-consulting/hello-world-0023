c731f336b67c7e93889e6df761227af1793eef10
Zach Overhulser <zoverhulser@gmail.com>
2018-05-09 17:58:13 -0500

    Greets users in Spanish.

--- greeting.mjs
+++ greeting.mjs
@@ -1,4 +1,7 @@
-export function makeGreeting(name = "world") {
+export function makeGreeting(name = "world", language = "English") {
+    if (language === "Spanish") {
+        return "¡Hola mundo!";
+    }
     return "Hello " + name + "!";
 }
 

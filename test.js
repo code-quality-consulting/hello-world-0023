49903a611bf60fed115de81abd2eaff7d6e7d66c
Zach Overhulser <zoverhulser@gmail.com>
2018-05-09 15:29:26 -0500

    Greets user personally.

--- test.js
+++ test.js
@@ -5,13 +5,16 @@ const assert = require("assert");
 
 // https://nodejs.org/docs/latest-v8.x/api/assert.html
 
-function makeGreeting() {
+function makeGreeting(name) {
     "use strict";
+    if (name) {
+        return "Hello " + name + "!";
+    }
     return "Hello world!";
 }
 
 // https://nodejs.org/docs/latest-v8.x/api/assert.html#assert_assert_strictequal_actual_expected_message
 
 assert.strictEqual(makeGreeting(), "Hello world!");
-assert.strictEqual(makeGreeting(), "Hello Mike!");
+assert.strictEqual(makeGreeting("Mike"), "Hello Mike!");
 console.log("All tests have passed.");
